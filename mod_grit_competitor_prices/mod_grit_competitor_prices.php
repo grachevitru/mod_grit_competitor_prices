deedbac365f70006da743c06bba84486e9f5f169
grachevitru <grachevitru@gmail.com>
2026-04-03 15:33:18 +0000

    Fix component SQL schema paths and relax default module filtering

--- mod_grit_competitor_prices/mod_grit_competitor_prices.php
+++ mod_grit_competitor_prices/mod_grit_competitor_prices.php
@@ -53,7 +53,7 @@ if (empty($analogs)) {
     return;
 }
 
-$hideLowerThanMyPrice = (bool) $params->get('hide_lower_than_my_price', 1);
+$hideLowerThanMyPrice = (bool) $params->get('hide_lower_than_my_price', 0);
 
 foreach ($analogs as $key => $item) {
     if (!isset($item->price)) {
