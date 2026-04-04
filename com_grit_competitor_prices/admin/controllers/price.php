4699a8a624e20e46ae932b93a3bc99c09536e2d4
grachevitru <grachevitru@gmail.com>
2026-04-04 14:34:00 +0000

    Add legacy controller compatibility for admin component tasks

---
+++ com_grit_competitor_prices/admin/controllers/price.php
@@ -0,0 +1,9 @@
+<?php
+
+defined('_JEXEC') or die;
+
+use Grit\Component\CompetitorPrices\Administrator\Controller\PriceController as NamespacedPriceController;
+
+class Grit_competitor_pricesControllerPrice extends NamespacedPriceController
+{
+}
---
+++ com_grit_competitor_prices/admin/controllers/prices.php
@@ -0,0 +1,9 @@
+<?php
+
+defined('_JEXEC') or die;
+
+use Grit\Component\CompetitorPrices\Administrator\Controller\PricesController as NamespacedPricesController;
+
+class Grit_competitor_pricesControllerPrices extends NamespacedPricesController
+{
+}
