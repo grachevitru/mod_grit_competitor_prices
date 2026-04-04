fe0671c913bc25e9127a4ddc751be96524bb061c
grachevitru <grachevitru@gmail.com>
2026-04-04 12:32:13 +0000

    Fix MySQL-compatible migration for competitor_url column

--- com_grit_competitor_prices/admin/sql/updates/mysql/1.0.1.sql
+++ com_grit_competitor_prices/admin/sql/updates/mysql/1.0.1.sql
@@ -1,2 +1,2 @@
 ALTER TABLE `#__competitor_prices`
-  ADD COLUMN IF NOT EXISTS `competitor_url` varchar(1024) DEFAULT NULL AFTER `competitor_name`;
+  ADD COLUMN `competitor_url` varchar(1024) DEFAULT NULL AFTER `competitor_name`;
